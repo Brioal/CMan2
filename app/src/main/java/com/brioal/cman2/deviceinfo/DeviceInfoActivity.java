82104fdf5c5cc7b26d62ddbb78b5b4bcd04e2cac
Brioal <brioal@foxmail.com>
2017-02-20 11:56:47 +0800

    +CMAN信息界面

---
+++ app/src/main/java/com/brioal/cman2/deviceinfo/DeviceInfoActivity.java
@@ -0,0 +1,15 @@
+package com.brioal.cman2.deviceinfo;
+
+import android.os.Bundle;
+
+import com.brioal.cman2.R;
+import com.brioal.cman2.base.BaseActivity;
+
+public class DeviceInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_device_info);
+    }
+}
